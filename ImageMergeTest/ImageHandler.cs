faa41d8978172cd74ce3add2315a6a1eb3f60a3f
3rabbs <thr333rabbit@gmail.com>
2024-11-10 00:01:12 -0600

    Add files via upload

---
+++ ImageMergeTest/ImageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMergeTest
+{
+    public class ImageHandler
+    {
+
+        Bitmap bitmap = new Bitmap(128, 128);  //base image to add layers onto
+        Random rand = new Random(); //used to randomly select images from a directory
+
+        Bitmap[] hats = new Bitmap[4];
+
+        
+
+
+    }
+}
---
+++ ImageMergeTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace ImageMerge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String sourceDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName;
+        
+            String path = sourceDir + "\\res\\Characters\\male\\";
+            Bitmap bitmap = new Bitmap(128, 128);  //base image to add layers onto
+            Random rand = new Random(); //used to randomly select images from a directory
+
+
+            List<Bitmap> hats = new List<Bitmap>();
+            
+            hats.Add(new Bitmap(path + "hats\\hats1.png"));
+            hats.Add(new Bitmap(path + "hats\\hats2.png"));
+
+
+
+            List<Bitmap> faces = new List<Bitmap>();
+
+            faces.Add(new Bitmap(path + "faces\\faces1.png"));
+            faces.Add(new Bitmap(path + "faces\\faces2.png"));
+
+
+
+            rand.Next(0, hats.Count);
+            Bitmap image = hats[Convert.ToInt32(rand)];
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                
+                g.DrawImage(image, 0, 0);
+           
+            }
+
+            
+            rand.Next(0, faces.Count);
+            image = faces[Convert.ToInt32(rand)];
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(image, 0, 0);
+
+            }
+
+
+            image.Save(path + "\\res\\Charcters");
+
+        }
+    }
+
+}
